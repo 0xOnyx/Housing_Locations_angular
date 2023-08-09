f5d5a52a0642fd3ee0f52b04195204b6b64b90e8
0xOnyx <60154937+0xOnyx@users.noreply.github.com>
2023-08-09 15:38:17 +0200

    2 files modified, 9 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Hello world!</h1>`,
+  imports: [HomeComponent, RouterModule],
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo"   aria-hidden="true">
+      </header>
+    </a>
+    
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
